c77f94d7c9e9dd93fa7118a7aced23e6d79cdd81
Sstark97 <aitorscinfo@gmail.com>
2022-10-19 11:03:27 +0100

    Start with js

--- 18-10-22/js/index.js
+++ 18-10-22/js/index.js
@@ -1,1 +1,30 @@
 // Parte de DEW
+{/* <div class="alert alert-warning alert-dismissible fade" role="alert">
+<strong>Holy guacamole!</strong> You should check in on some of those fields below.
+<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div> */}
+
+/**
+ * Funcion que nos genera un alert de Bootstrap
+ * @return Componente Alert de Boostrap
+ */
+const createBootsrapAlert = () => {
+    const alert = document.createElement("div")
+    const pAlert = document.createElement("p")
+    const btnAlert = document.createElement("button")
+
+    alert.classList = "alert alert-warning alert-dismissible fade show"
+    alert.role = "alert"
+
+    btnAlert.classList = "btn-close"
+    btnAlert.dataset.bsDismiss = "alert"
+    btnAlert.ariaLabel = "Close"
+
+    alert.append(pAlert, btnAlert)
+
+    return alert
+}
+
+const loginModal = document.querySelector("#login_modal")
+const inputsLogin = document.querySelectorAll("#login_modal input")
+
