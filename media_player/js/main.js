155234f7837b29b85e2b596681b0058b1bcf509d
Sstark97 <aitorscinfo@gmail.com>
2023-01-14 10:39:35 +0000

    Add media player first styles

---
+++ media_player/js/main.js
@@ -0,0 +1,8 @@
+const main = document.querySelector("main")
+
+// window.addEventListener("load", ()=> {
+//     const videoPlayer = document.createElement("video")
+//     videoPlayer.controls = true
+
+//     main.append(videoPlayer)
+// })
