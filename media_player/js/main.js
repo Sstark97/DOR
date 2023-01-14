82a4cba24a47b3cf28c21cac0d464e0b4d0b3592
Sstark97 <aitorscinfo@gmail.com>
2023-01-14 11:38:00 +0000

    Add play-pause functions

--- media_player/js/main.js
+++ media_player/js/main.js
@@ -1,4 +1,21 @@
 const main = document.querySelector("main")
+const video = document.querySelector("video")
+const controls = document.querySelector("#controls")
+const playBtn = document.querySelector("#play")
+
+console.log(video)
+
+playBtn.addEventListener("click", () => {
+    const playIcon = playBtn.firstChild.nextSibling
+
+    if(video.paused) {
+        playIcon.className = playIcon.className.replace("bx-play", "bx-pause")
+        video.play()
+    } else {
+        playIcon.className = playIcon.className.replace("bx-pause", "bx-play")
+        video.pause()
+    }
+})
 
 // window.addEventListener("load", ()=> {
 //     const videoPlayer = document.createElement("video")
