19d488b7695ed2692c7fa74c18eef70e44b22968
Sstark97 <aitorscinfo@gmail.com>
2023-01-14 11:49:55 +0000

    Add video duration and time progress

--- media_player/js/main.js
+++ media_player/js/main.js
@@ -2,8 +2,32 @@ const main = document.querySelector("main")
 const video = document.querySelector("video")
 const controls = document.querySelector("#controls")
 const playBtn = document.querySelector("#play")
+const timeProgress = document.querySelector("#timeProgress")
+const videoDuration = document.querySelector("#duration")
 
 console.log(video)
+
+const formatTime = time => {
+    let seconds = Math.floor(time % 60),
+    minutes = Math.floor(time / 60) % 60,
+    hours = Math.floor(time / 3600);
+
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    hours = hours < 10 ? `0${hours}` : hours;
+
+    if(hours == 0) {
+        return `${minutes}:${seconds}`
+    }
+    return `${hours}:${minutes}:${seconds}`;
+}
+
+video.addEventListener("timeupdate", e => {
+    let {currentTime, duration} = e.target;
+    let percent = (currentTime / duration) * 100;
+    timeProgress.style.width = `${percent}%`;
+    videoDuration.textContent = formatTime(currentTime);
+});
 
 playBtn.addEventListener("click", () => {
     const playIcon = playBtn.firstChild.nextSibling
