a65f8314211d9177a13a6ebe354704d1d776973f
Sstark97 <aitorscinfo@gmail.com>
2023-01-15 19:48:45 +0000

    Add change video in media plaer

--- media_player/js/const.js
+++ media_player/js/const.js
@@ -19,6 +19,46 @@ const videoActions = {
     }
 }
 
+const videos = [
+    {
+        id: 0,
+        title: "Quevedo: Bzrp Music Sessions, Vol. 52",
+        author: "Bizarrap, Quevedo",
+        src: [
+            "./media/video/bzr_quevedo.mp4"
+        ],
+        poster: "./media/video/poster/poster_quevedo.png"
+    },
+    {
+        id: 1,
+        title: "Shakira: Bzrp Music Sessions, Vol. 53",
+        author: "Bizarrap, Shakira",
+        src: [
+            "./media/video/bzr_shakira.mp4"
+        ],
+        poster: "./media/video/poster/poster_shakira.png"
+    },
+    {
+        id: 2,
+        title: "Duki: Bzrp Music Sessions, Vol. 50",
+        author: "Bizarrap, Duki",
+        src: [
+            "./media/video/bzr_duki.mp4"
+        ],
+        poster: "./media/video/poster/poster_duki.png"
+    },
+    {
+        id: 3,
+        title: "Heladio: Bzrp Music Sessions, Vol. 52",
+        author: "Bizarrap, Heladio",
+        src: [
+            "./media/video/bzr_heladio.mp4"
+        ],
+        poster: "./media/video/poster/poster_heladio.png"
+    },
+]
+
 export {
-    videoActions
+    videoActions,
+    videos
 }
--- media_player/js/main.js
+++ media_player/js/main.js
@@ -1,17 +1,68 @@
 import { formatTime } from "./functions.js"
-import { videoActions } from "./const.js"
+import { videoActions, videos } from "./const.js"
 
-const main = document.querySelector("main")
+const trackList = document.querySelector("#trackList")
 const video = document.querySelector("video")
 const controls = document.querySelector("#controls")
 const timeProgress = document.querySelector("#timeProgress")
 const videoDuration = document.querySelector("#duration")
 const volumeControl = document.querySelector("#volumeControl")
 
+window.addEventListener("load", () => {
+    const tracks = videos.map(track => {
+        const trackWrapper = document.createElement("div")
+        const trackInfo = document.createElement("div")
+        const poster = document.createElement("img")
+        const title = document.createElement("h3")
+        const author = document.createElement("p")
+
+        poster.src = track.poster
+        title.textContent = track.title
+        author.textContent = track.author
+
+        poster.id = track.id
+
+        trackWrapper.className = "flex hover:cursor-pointer"
+        trackInfo.className = "ml-5"
+        poster.className = "w-[35%] rounded"
+        title.className = "max-w-[20ch] font-bold"
+        author.className = "text-sm font-lighter text-slate-600"
+
+        trackInfo.append(title, author)
+        trackWrapper.append(poster, trackInfo)
+
+        return trackWrapper
+    })
+
+    trackList.append(...tracks)
+})
+
 controls.addEventListener("click", e => {
     const element = e.target
 
-    videoActions[element.id](video, element)
+    if(videoActions[element.id]) {
+        videoActions[element.id](video, element)
+    }
+})
+
+trackList.addEventListener("click", e => {
+    const element = e.target
+
+    if(videos[element.id]) {
+        const source = video.firstElementChild
+        const playIcon = document.querySelector("#play")
+        const videoInfo = document.querySelector("#videoInfo")
+
+        playIcon.className = "bx bx-play text-4xl"
+        timeProgress.style.width = "auto"
+
+        videoInfo.firstElementChild.textContent = videos[element.id].title
+        videoInfo.lastElementChild.textContent = videos[element.id].author
+        video.poster = videos[element.id].poster
+        source.src = videos[element.id].src
+
+        video.load()
+    }
 })
 
 volumeControl.addEventListener("change", () => {
@@ -27,10 +78,3 @@ video.addEventListener("timeupdate", e => {
     timeProgress.style.width = `${percent}%`;
     videoDuration.textContent = formatTime(currentTime);
 });
-
-// window.addEventListener("load", ()=> {
-//     const videoPlayer = document.createElement("video")
-//     videoPlayer.controls = true
-
-//     main.append(videoPlayer)
-// })
